9cd5c8b763c13897892ccc23022b7a04bf2c035e
Irfan Hussain <irfanhussains98786@gmail.com>
2025-09-10 20:26:13 +0530

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,22 @@ import { initializeKnowledgeBase } from './services/knowledgeBaseService.js';
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-const corsOptions = {
-  origin: 'https://legalallyai.vercel.app',
-  optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
+const allowedOrigins = [
+  'https://legalallyai.vercel.app',
+  'http://localhost:5173'
+];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api', analyzeRoutes);
